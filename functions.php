06cb26dc8303b9905d10d80dc4c5744367362c2d
Sharaz Shahid <bc140401486@vu.edu.pk>
2020-12-05 15:41:14 +0500

    js path correction

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function hampres_child_enqueue_child_styles() {
 		);
 		wp_enqueue_style( 'childe-style' );
 
-		wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . 'assets/js/custom.js', [ 'jquery' ], '1.0.0' );
+		wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', [ 'jquery' ], '1.0.0' );
 }
 
 add_action( 'wp_enqueue_scripts', 'hampres_child_enqueue_child_styles' );
